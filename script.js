e8b9a6babbe9ea8c0013a5352e857153da3d84b4
payce06 <paycegrossman@gmail.com>
2025-06-08 16:53:39 -0400

    script-js

--- script.js
+++ script.js
@@ -17,3 +17,57 @@ function renderExpenses() {
     expenses.forEach((expense, index) => {
         const expenseElement = document.createElement('li');
         expenseElement.innerHTML = `
+        <div>
+            <span>${expense.name}</span>
+            <span>$${expense.amount}</span>
+            <span>${expense.category}</span>
+            <span>${expense.date}</span>
+        </div>
+        <button onclick="deleteExpense(${index})">Delete</button>
+        `;
+        expenseList.appendChild(expenseElement);
+        total += parseFloat(expense.amount);
+    });
+
+    totalExpensesSpan.textContent = total.toFixed(2);
+}
+
+function  saveExpenses(){
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+}
+addExpenseButton.addEventListener('click', () => {
+    const name = expenseNameInput.value;
+    const amount = parseFloat(expenseAmountInput.value);
+    const date = expenseDateInput.value;
+    const category = expenseCategorySelect.value;
+
+    if(!name || !amount || !date) {
+        alert('Please fill in all fields');
+        return;
+    }
+
+    const newExpense = {name, amount, date, category};
+    expenses.push(newExpense);
+    saveExpenses();
+    renderExpenses();
+
+   expenseNameInput.value = '';
+   expenseAmountInput.value = '';
+   expenseDateInput.value = '';
+   expenseCategorySelect.value = 'Food';
+});
+
+function deleteExpense(index) {
+    expenses.splice(index, 1);
+    saveExpenses();
+    renderExpenses();
+}
+
+categoryFilterSelect.addEventListener('change', () => {
+    const category = categoryFilterSelect.value;
+    const filteredExpenses = category == 'All'
+       ? expenses
+       : expenses.filter(expense => expense.category == category);
+
+    renderFilteredExpenses(filteredExpenses);
+});
